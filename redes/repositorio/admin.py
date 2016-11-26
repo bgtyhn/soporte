2c9e4b26e09b05a87c858e5d57fa5d6e4b5ceed4
bgtyhn <juangamix@gmail.com>
2016-11-26 18:43:51 -0500

    verificacion de tokens V0.001

--- redes/repositorio/admin.py
+++ redes/repositorio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Token
+
+admin.site.register(Token)
 # Register your models here.
--- redes/repositorio/models.py
+++ redes/repositorio/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Token(models.Model):
+	token = models.CharField(max_length = 5000)
+	fileAddress = models.CharField(max_length = 5000)
+
 # Create your models here.
