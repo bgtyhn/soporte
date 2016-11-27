62c304593102ad9d626b5a80a3cec2fdc64ae8e1
bgtyhn <juangamix@gmail.com>
2016-11-26 19:48:32 -0500

    manejo 404 V0.001

--- redes/repositorio/views.py
+++ redes/repositorio/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.template import loader, RequestContext
+from django.shortcuts import render_to_response
 from .models import Token
 
 def index(request):
@@ -11,5 +12,11 @@ def index(request):
 	print(redirect)
 	context['redirect'] = redirect
 	return HttpResponse(template.render(context, request))
+
+def handler404(request):
+    response = render_to_response('redes/404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
 
 # Create your views here.
