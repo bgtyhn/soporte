6b938c530054acb630ce57cd44e3076cc87997ae
bgtyhn <juangamix@gmail.com>
2016-11-26 19:44:15 -0500

    cambios index

--- redes/repositorio/views.py
+++ redes/repositorio/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Token
 
 def index(request):
 	template = loader.get_template('redes/index.html')
 	context = {}
+	token = request.GET['token']
+	print(token)
+	redirect = Token.objects.get(token = token);
+	print(redirect)
+	context['redirect'] = redirect
 	return HttpResponse(template.render(context, request))
 
 # Create your views here.
